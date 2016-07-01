a27c0456e90a24f0dac0d01f35d007eb3ec9196a
unknown <>
2016-07-01 15:04:57 +0900

    ログへの出力のテスト

---
+++ app/src/main/java/jp/techacademy/takefumi/oonishi/javalog/MainActivity.java
@@ -0,0 +1,17 @@
+package jp.techacademy.takefumi.oonishi.javalog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("javatest","ログへの出力テスト");
+
+
+    }
+}
